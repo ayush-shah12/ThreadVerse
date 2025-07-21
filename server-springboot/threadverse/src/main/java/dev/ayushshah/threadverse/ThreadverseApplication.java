2815d08ae729aa1e08f36c3726d63149264ab203
Ayush <ayushsh285@gmail.com>
2025-07-21 19:46:07 -0400

    springboot setup

---
+++ server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/ThreadverseApplication.java
@@ -0,0 +1,13 @@
+package dev.ayushshah.threadverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreadverseApplication.class, args);
+	}
+
+}
---
+++ server-springboot/threadverse/src/test/java/dev/ayushshah/threadverse/ThreadverseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ayushshah.threadverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
