618bc88be887f6a26b9a96ad2a2fdd451751fe7d
Ayush <ayushsh285@gmail.com>
2025-07-26 13:26:18 -0400

    clean up

--- server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/controller/UserController.java
+++ server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/controller/UserController.java
@@ -3,9 +3,7 @@ package dev.ayushshah.threadverse.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
@@ -15,24 +13,19 @@ import org.springframework.security.core.Authentication;
 
 import jakarta.servlet.http.HttpServletResponse;
 
-import dev.ayushshah.threadverse.dto.UserDTO;
-import dev.ayushshah.threadverse.repository.UserRepository;
-import dev.ayushshah.threadverse.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
 
+import dev.ayushshah.threadverse.dto.UserDTO;
+import dev.ayushshah.threadverse.service.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    private final UserRepository userRepository;
-    
     private final UserService userService;
 
-    public UserController(UserService userService, UserRepository userRepository){
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.userRepository = userRepository;
     }
 
     @PostMapping("/me")
@@ -43,20 +36,20 @@ public class UserController {
 
     @PostMapping("/logout")
     public ResponseEntity<?> logout(Authentication auth, HttpServletResponse res) {
-        ResponseCookie cookie = ResponseCookie.from("token", "")       
-        .httpOnly(true)
-        // .secure(true)
-        .path("/")
-        .maxAge(0)
-        .sameSite("Strict")
-        .build();
+        ResponseCookie cookie = ResponseCookie.from("token", "")
+                .httpOnly(true)
+                // .secure(true)
+                .path("/")
+                .maxAge(0)
+                .sameSite("Strict")
+                .build();
 
         res.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
 
         String userId = ((UserDTO) auth.getPrincipal()).getId();
 
         return ResponseEntity.ok("User: " + userId + " has been logged out.");
-        
+
     }
 
     @PreAuthorize("hasRole('ADMIN')")
@@ -64,11 +57,5 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
-    
-    
-    
-
 
 }
-
-
