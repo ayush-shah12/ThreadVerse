f6ebee22cd4714e9391c02df60308ea06e5e4143
Ayush <ayushsh285@gmail.com>
2025-07-26 13:24:01 -0400

    admin route: get all users

--- server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/controller/UserController.java
+++ server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/controller/UserController.java
@@ -5,25 +5,34 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 
 import jakarta.servlet.http.HttpServletResponse;
 
 import dev.ayushshah.threadverse.dto.UserDTO;
+import dev.ayushshah.threadverse.repository.UserRepository;
 import dev.ayushshah.threadverse.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    private final UserRepository userRepository;
     
     private final UserService userService;
 
-    public UserController(UserService userService){
+    public UserController(UserService userService, UserRepository userRepository){
         this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/me")
@@ -49,6 +58,13 @@ public class UserController {
         return ResponseEntity.ok("User: " + userId + " has been logged out.");
         
     }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+    
     
     
 
