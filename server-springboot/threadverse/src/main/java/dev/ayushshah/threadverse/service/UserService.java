3a8c9330729a78da4de2c7568e0b84fd3e3478e9
Ayush <ayushsh285@gmail.com>
2025-07-26 13:27:25 -0400

    remove unused imports

--- server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/service/UserService.java
+++ server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/service/UserService.java
@@ -1,8 +1,6 @@
 package dev.ayushshah.threadverse.service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
