9af5b15672f2976decc9ddb84dce8e319542cddf
Ayush <ayushsh285@gmail.com>
2025-07-27 16:31:42 -0400

    post controller

---
+++ server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/controller/PostController.java
@@ -0,0 +1,56 @@
+package dev.ayushshah.threadverse.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.security.core.Authentication;
+
+import dev.ayushshah.threadverse.dto.CreatePostRequest;
+import dev.ayushshah.threadverse.dto.PostWithUserDTO;
+import dev.ayushshah.threadverse.dto.UserDTO;
+import dev.ayushshah.threadverse.model.Post;
+import dev.ayushshah.threadverse.service.PostService;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/post")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<PostWithUserDTO>> getAllPosts() {
+        return ResponseEntity.ok(postService.getAllPosts());
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<PostWithUserDTO> getPostById(@PathVariable String postId) {
+        Optional<PostWithUserDTO> postOpt = postService.getPostById(postId);
+        return postOpt
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/byCommunity/{communityId}")
+    public ResponseEntity<List<PostWithUserDTO>> getPostsByCommunity(@PathVariable String communityId) {
+        return ResponseEntity.ok(postService.getPostsByCommunityId(communityId));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Post> createPost(@RequestBody CreatePostRequest postRequest, Authentication auth) {
+        return ResponseEntity.ok(postService.createPost(postRequest, ((UserDTO) auth.getPrincipal()).getId()));
+    }
+
+}
