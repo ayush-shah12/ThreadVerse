930785a51a9e9c9b655310455e9e5c71a39269bd
Ayush <ayushsh285@gmail.com>
2025-07-28 16:18:46 -0400

    update vote model

--- server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/model/PostVote.java
+++ server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/model/PostVote.java
@@ -1,12 +1,14 @@
 package dev.ayushshah.threadverse.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,9 +17,10 @@ public class PostVote {
     @Id
     private String id;
 
-    private String userID;
-    private String postID;
+    private String userId;
+    private String postId;
 
+    @Builder.Default
     private VoteType vote = VoteType.NOVOTE;
 
 }
