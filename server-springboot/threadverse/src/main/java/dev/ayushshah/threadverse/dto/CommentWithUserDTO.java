ad1fc35947f8490f396eeec5cdc7dceacbeb9945
Ayush <ayushsh285@gmail.com>
2025-07-28 19:20:07 -0400

    update models and dto

---
+++ server-springboot/threadverse/src/main/java/dev/ayushshah/threadverse/dto/CommentWithUserDTO.java
@@ -0,0 +1,27 @@
+package dev.ayushshah.threadverse.dto;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class CommentWithUserDTO {
+    @Id
+    private String id;
+    private String content;
+
+    private int votes;
+
+    private Date date;
+
+    private String authorID;
+    private List<String> replyIds;
+
+    private UserDTO user;
+
+}
